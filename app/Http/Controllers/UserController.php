cb93b679c5d629efe8cac91b79dfb0038a68963e
Athallah Adjani Prasanna Basuki <100335035+ATHDJANI@users.noreply.github.com>
2024-03-11 21:06:00 +0700

    PRAKTIKUM 6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    public function index(){
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        //    return view('user', ['data' => $user]);
+
+         $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ];
+         UserModel::insert($data); // tambahkan data ke tabel m_user
+        //coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+
+}
