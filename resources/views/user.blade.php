9841bcc9c8193ba356bb46a212b7b3fb7d1127ca
Athallah Adjani Prasanna Basuki <100335035+ATHDJANI@users.noreply.github.com>
2024-03-19 22:21:08 +0700

    Prac 2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,19 +7,25 @@
 <h1>Data User</h1>
 <table border="1" cellpadding="2" cellspacing="0">
 <tr>
-<th>ID</th>
-<th>Username</th>
+{{-- <th>ID</th> --}}
+{{-- <th>Username</th>
 <th>Nama</th>
-<th>ID Level Pengguna</th>
+<th>ID Level Pengguna</th> --}}
 </tr>
-@foreach ($data as $d)
-<tr>
+{{-- @foreach ($data as $d) --}}
+{{--<tr>
 <td>{{ $data->user_id}}</td> 
 <td>{{$data->username }}</td>
 <td>{{$data->nama }}</td>
 <td>{{$data->level_id }}</td>
+</tr>--}}
+<tr>
+<td>Jumlah Pengguna</td>
 </tr>
-@endforeach
+<tr>
+<td>{{ $data }}</td>
+</tr>
+{{-- @endforeach --}}
 </table>
 </body>
 </html>
